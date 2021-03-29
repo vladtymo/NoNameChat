a8ac868f0d3250aadb110cebe808668de5fbb457
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2021-03-29 21:36:55 +0300

    First step : Little Model with static encrypt in RegisterService.cs

---
+++ DAL/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL
+{
+    using System.Collections.Generic;
+
+    public class Admin
+    {
+
+        public Admin()
+        {
+            Users = new HashSet<User>();
+            Groups = new HashSet<Group>();
+        }
+        public int Id { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
---
+++ DAL/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL
+{
+    using System.Collections.Generic;
+
+    public class Group
+    {
+        public Group()
+        {
+            Users = new HashSet<User>();
+            Admins = new HashSet<Admin>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Admin> Admins { get; set; }
+    }
+}
