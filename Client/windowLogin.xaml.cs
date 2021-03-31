4e42666991f109cfee5359d8a5b3bb396f1eb19c
Ned4ik <bohdan.vozniuk@gmail.com>
2021-03-31 16:30:22 +0300

    Added login window and integrated material design on project

---
+++ Client/windowLogin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for windowLogin.xaml
+    /// </summary>
+    public partial class windowLogin : Window
+    {
+        public windowLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void PackIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}
